766f56b7ff22c9592da116769899f8cb70517cc1
acv-tamhc <acv-tamhc@atmarkcafe.org>
2018-01-10 15:43:43 +0700

    add references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110083523) do
+ActiveRecord::Schema.define(version: 20180110084209) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20180110083523) do
     t.decimal "price", precision: 6, scale: 2
     t.boolean "published"
     t.string "short_decription"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
 end
