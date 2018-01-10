d5dcb34727c03e46b8dc649b50690d70fd983366
acv-tamhc <acv-tamhc@atmarkcafe.org>
2018-01-10 15:39:26 +0700

    migrate -- add table 'categories'

---
+++ db/migrate/20180110083523_add_table_categories.rb
@@ -0,0 +1,9 @@
+class AddTableCategories < ActiveRecord::Migration[5.1]
+  def change
+    create_table :categories do |t|
+      t.string :title
+      # t.text   :description
+      t.timestamps
+    end
+  end
+end
