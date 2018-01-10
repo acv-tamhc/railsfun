5fcee3d369fbf8a00733be86f4b9442450f2551a
acv-tamhc <acv-tamhc@atmarkcafe.org>
2018-01-10 16:23:01 +0700

    change database

--- db/migrate/20180110045414_add_fields_to_products.rb
+++ db/migrate/20180110045414_add_fields_to_products.rb
@@ -2,6 +2,6 @@ class AddFieldsToProducts < ActiveRecord::Migration[5.1]
   def change
     add_column :products, :price, :decimal, precision: 6, scale: 2
     add_column :products, :published, :boolean
-    add_column :products, :short_decription, :string
+    add_column :products, :short_description, :string
   end
 end
